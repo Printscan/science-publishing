c226d9316f73e7ca767e3c9af4c0d487746b1481
Übermensch <59955405+Printscan@users.noreply.github.com>
2025-11-26 08:41:30 +0300

    fix: adjust app path after move and fix tests import

--- api/apps.py
+++ api/apps.py
@@ -3,7 +3,8 @@ from django.apps import AppConfig
 
 class SciencePublishingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.modules.science_publishing'
+    # путь к приложению после переноса в modules/science_publishing/api
+    name = 'modules.science_publishing.api'
     label = 'science_publishing'
     verbose_name = 'Управление научными публикациями'
     auto_api = True
